462ea0ef2aa7caa9c1d8a6ae56922bac566766b1
EvgeniIvanov <evh_iv@yahoo.com>
2014-03-18 15:57:21 +0200

    Added percent of month

--- BankApplication/src/bank/account/Percentage.java
+++ BankApplication/src/bank/account/Percentage.java
@@ -1,4 +1,3 @@
-
 package bank.account;
 
 import bank.technical.Timer;
@@ -16,24 +15,27 @@ public class Percentage {
         this.balance = b;
         this.count = c;
         this.month = m;
-        percentage();
-    }
-
-    private void percentage() {
-        //if(timer.getTime() == 12){
         try {
             timer.startTime(month);
         } catch (InterruptedException ex) {
             Logger.getLogger(Deposit.class.getName()).log(Level.SEVERE, null, ex);
         }
+        percentage(timer.getI());
+    }
+
+    private void percentage(int i) {
+        System.out.println("Timer " + timer.getI());
+        //if(timer.getTime() == 12){
+        
         count = (balance / 100) * 11;
+        count = (count / 12) * i;
         //}
     }
-    
+
     public double getCountPercentage() {
         return count;
     }
-    
+
     public double getBalance() {
         return balance;
     }
--- BankApplication/src/bank/technical/Timer.java
+++ BankApplication/src/bank/technical/Timer.java
@@ -2,15 +2,68 @@ package bank.technical;
 
 public class Timer {
 
-    public void startTime(String month) throws InterruptedException {
-        System.out.println(month);
-        int i = 1;
-        while (i < 2) {
-            if (i < 2) {
+    private int monthNumber;
+
+    public int startTime(String month) throws InterruptedException {
+
+        int i = 0;
+        
+        switch (month) {
+            case "January":
+                i = 1;
+                break;
+            case "February":
+                i = 2;
+                break;
+            case "March":
+                i = 3;
+                break;
+            case "April":
+                i = 4;
+                break;
+            case "May":
+                i = 5;
+                break;
+            case "June":
+                i = 6;
+                break;
+            case "July":
+                i = 7;
+                break;
+            case "August":
+                i = 8;
+                break;
+            case "September":
+                i = 9;
+                break;
+            case "October":
+                i = 10;
+                break;
+            case "November":
+                i = 11;
+                break;
+            case "December":
+                i = 12;
+                break;
+        }
+
+        i = 13 - i;
+        monthNumber = i;
+
+        while (i < 13) {
+            if (i < 13) {
                 Thread.sleep(1000);
+                System.out.println(i);
             }
             i++;
-        }
+        }      
+        return monthNumber;
+    }
+    
+
+    public int getI() {
+        System.out.println("month2 " + monthNumber);
+        return monthNumber;
     }
 
 }
